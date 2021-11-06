38ab6ee5aabfa514284cd37e317e12c158f7babd
Jhon Aparicio <jj-aparicior@hotmail.com>
2021-11-06 14:58:00 -0500

    models, controllers and repositorys update

---
+++ src/models/servicio.model.ts
@@ -0,0 +1,50 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Cliente} from './cliente.model';
+import {Encomienda} from './encomienda.model';
+
+@model()
+export class Servicio extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  hora: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  valor: number;
+
+  @belongsTo(() => Cliente, {name: 'origenFk'})
+  origen: string;
+
+  @belongsTo(() => Cliente, {name: 'destinoFk'})
+  destino: string;
+
+  @belongsTo(() => Encomienda, {name: 'encomiendaFk'})
+  encomienda: string;
+
+  constructor(data?: Partial<Servicio>) {
+    super(data);
+  }
+}
+
+export interface ServicioRelations {
+  // describe navigational properties here
+}
+
+export type ServicioWithRelations = Servicio & ServicioRelations;
